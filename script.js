1f6ca73dc44ec89e9955f5afea77e3e2fdb3e3a4
salikwani <120441251+salikwani@users.noreply.github.com>
2023-06-02 17:32:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+const signupBtn = document.getElementById('signup-btn');
+const loginBtn = document.getElementById('login-btn');
+
+signupBtn.addEventListener('click', (e) => {
+    var link = document.createElement('a');
+    link.href = "../signup/index.html";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
+
+loginBtn.addEventListener('click', () => {
+    var link = document.createElement('a');
+    link.href = "../login/index.html";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
