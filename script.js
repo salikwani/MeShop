909f2c98a09486051157eb51b0276669b08c1a68
salikwani <120441251+salikwani@users.noreply.github.com>
2023-06-02 21:05:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const loginBtn = document.getElementById('login-btn');
 
 signupBtn.addEventListener('click', (e) => {
     var link = document.createElement('a');
-    link.href = "../signup/index.html";
+    link.href = "./signup/index.html";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -13,7 +13,7 @@ signupBtn.addEventListener('click', (e) => {
 
 loginBtn.addEventListener('click', () => {
     var link = document.createElement('a');
-    link.href = "../login/index.html";
+    link.href = "./login/index.html";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
